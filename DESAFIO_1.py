d1a29b8418247758cfa51510183d0c90c08753f5
rai-com-i <151753763+rai-com-i@users.noreply.github.com>
2024-09-04 09:57:55 -0300

    Update DESAFIO_1.py

--- DESAFIO_1.py
+++ DESAFIO_1.py
@@ -4,11 +4,8 @@ def pertence_fibonacci(numero):
         a, b = b, a + b
     return b == numero
 
-# Obtém o número do usuário
 numero = int(input("Digite um número: "))
-numero = 13
 
-# Verifica se o número pertence à sequência e exibe a mensagem correspondente
 if pertence_fibonacci(numero):
     print(f"O número {numero} pertence à sequência de Fibonacci!")
 else:
